16ffef57be6f8dff9eec7d914a267a24c88def14
Ankit Arora <ankit@Ankits-MacBook-Pro-2.local>
2025-02-06 15:49:07 +0530

    Updated notifications component to include bookmarks for notifications

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react";
+import { Bookmark } from "../types/Bookmark";
+import { getAllBookmarksForNotification } from "../chrome/storage";
+import Card from "./Card";
+
 const Notifications = () => {
-  return <div></div>;
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [bookmarks, setBookmarks] = useState<Bookmark[]>([]);
+
+  useEffect(() => {
+    async function getAllNotificationBookmarks() {
+      const notificationBookmarks: Bookmark[] =
+        await getAllBookmarksForNotification();
+
+      setBookmarks(notificationBookmarks);
+      setIsLoading(false);
+    }
+
+    getAllNotificationBookmarks();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      {bookmarks.map((bookmark) => (
+        <Card bookmark={bookmark} />
+      ))}
+    </div>
+  );
 };
 
 export default Notifications;
